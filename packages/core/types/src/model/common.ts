d80a337a996f5c2576abbd8263e647e32abc772a
sleepyfran <fgzv@outlook.com>
2024-06-10 00:22:01 +0200

    Add base models for track and artist

--- packages/core/types/src/model/common.ts
+++ packages/core/types/src/model/common.ts
@@ -1,4 +1,5 @@
 import { Brand } from "effect";
+import * as S from "@effect/schema/Schema";
 
 /**
  * Wrapper around a string to indicate that it represents a GUID. It does not
@@ -6,3 +7,10 @@ import { Brand } from "effect";
  */
 export type Guid = string & Brand.Brand<"Guid">;
 export const Guid = Brand.nominal<Guid>();
+
+/**
+ * Wrapper around a string to represent a generic ID. It does not perform any
+ * validation other than requiring the string to be non-empty.
+ */
+export const GenericId = S.String.pipe(S.nonEmpty());
+export type GenericId = S.Schema.Type<typeof GenericId>;
