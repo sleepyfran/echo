af0bb62064eefc93c5a0a100b5cd489c9291ebe4
sleepyfran <fgzv@outlook.com>
2024-09-27 11:31:58 +0200

    Partial implementation of Spotify into loader

---
+++ packages/infrastructure/spotify-provider/index.ts
@@ -0,0 +1,13 @@
+import { Layer } from "effect";
+import { SpotifyAuthenticationLive } from "./src/spotify-authentication";
+import { SpotifyProviderLive } from "./src/spotify-provider";
+
+/**
+ * Layer that can be used to construct the Spotify provider. The returned
+ * provider can then be used to retrieve the authentication provider
+ * (in this case, Spotify) or create a new instance of the Spotify provider
+ * given the authentication info returned by Spotify.
+ */
+export const SpotifyProviderFactoryLive = SpotifyProviderLive.pipe(
+  Layer.provide(SpotifyAuthenticationLive),
+);
--- packages/infrastructure/spotify-provider/src/spotify-provider.ts
+++ packages/infrastructure/spotify-provider/src/spotify-provider.ts
@@ -1,9 +1,15 @@
-import { MediaProviderFactory } from "@echo/core-types";
+import { MediaProviderFactory, ProviderType } from "@echo/core-types";
 import { Effect, Layer } from "effect";
 import { SpotifyAuthentication } from "./spotify-authentication";
 import { UserLibraryApi } from "./apis/user-library-api";
 import { createListAlbums } from "./apis/list-albums-api";
 
+/**
+ * Implementation of the Spotify provider that uses Spotify for authentication,
+ * library discovery and playback. Exports a layer that can be used to either
+ * get the authentication provider or create a new instance of the media provider
+ * based on the authentication info given by the auth provider.
+ */
 export const SpotifyProviderLive = Layer.effect(
   MediaProviderFactory,
   Effect.gen(function* () {
@@ -13,7 +19,7 @@ export const SpotifyProviderLive = Layer.effect(
     return MediaProviderFactory.of({
       authenticationProvider: Effect.succeed(spotifyAuth),
       createMediaProvider: (authInfo) => ({
-        _tag: "ApiBased",
+        _tag: ProviderType.ApiBased,
         listAlbums: createListAlbums(authInfo, userLibraryApi),
       }),
     });
